f2de05f36ed4655d03d3fdbf6fbe498b8450bfd7
Dinesh <dineshg.gowda@contentstack.com>
2019-09-05 18:27:42 +0530

    changes config

--- config/default.js
+++ config/default.js
@@ -11,7 +11,7 @@ module.exports = exports = {
 		"client_id": "x2z4Pthbd9Rt17C8QpsX3jOH",
 		"client_secret": "ca25HOYZoc5LggFNqosjGXmHSUDieKa8"
 	},
-	url:"localhost:4000"
-	//url:"https://commercetool-contentstack-demo.herokuapp.com",
+	//url:"localhost:4000"
+	url:"https://commercetool-contentstack-demo.herokuapp.com",
 	//url: "https://peaceful-thicket-75002.herokuapp.com/",
 };
