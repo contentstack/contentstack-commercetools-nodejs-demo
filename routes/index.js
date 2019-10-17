733441a9c23dedbafa14b0ab9deb8831d47f5c34
Dinesh <dineshg.gowda@contentstack.com>
2019-10-17 13:43:44 +0530

    dummy behaviour

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,11 @@ module.exports = (app) => {
 	app.use('/',require('../middlewares'));
 	app.use('/createOrder',createOrderFromCart);
 	app.use('/createCart',createCart);
-	app.use('/cart/:cartId/pay',getCartDetails);
+	app.use('/cart/:cartId/pay',(req,res) =>{
+		   res.render('pages/payment',{
+    			totalPrice:100,
+    		})
+	});
 	app.use('/', require('./home2'));
 	app.use('/category', require('./category'));
 	app.use('/product', require('./product'));
