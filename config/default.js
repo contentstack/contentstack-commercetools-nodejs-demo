2a94651ecbf6c9195e5d0d2fa2a76f19d663f8c4
Dinesh <dineshg.gowda@contentstack.com>
2019-10-19 15:26:24 +0530

    changes

--- config/default.js
+++ config/default.js
@@ -2,14 +2,14 @@ module.exports = exports = {
 	port: 4000,
 	// Contentstack Config
 	contentstack: {
-		api_key: 'bltd298a6d9667f450f',
-		access_token: 'bltc02816e8456c87a1'
+		api_key: '',
+		access_token: ''
 	},
 	commercetools: {
 		"project_key": "cs-ct-integration",
 		"host": "sphere.io",
-		"client_id": "MYcAin83IbNAErrGD-8lkP6-",
-		"client_secret": "0mFlDTtZaKX4qkBS6JJtLxMeZMQzgeaM",
+		"client_id": "",
+		"client_secret": "",
 		"scopes":['manage_project:cs-ct-integration']
 	}
 };
