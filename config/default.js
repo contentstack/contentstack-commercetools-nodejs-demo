b40701b98f95048e6b90c896191c4a1f1838b984
Dinesh <dineshg.gowda@contentstack.com>
2019-10-21 18:11:55 +0530

    linted js code

--- config/default.js
+++ config/default.js
@@ -1,15 +1,15 @@
 module.exports = exports = {
-	port: 4000,
-	// Contentstack Config
-	contentstack: {
-		api_key: '',
-		access_token: ''
-	},
-	commercetools: {
-		"project_id": "",
-		"host": "",  //api host url
-		"client_id": "",
-		"client_secret": "",
-		"scopes":[]
-	}
+  port: 4000,
+  // Contentstack Config
+  contentstack: {
+    api_key: '',
+    access_token: '',
+  },
+  commercetools: {
+    'project_id': '',
+    'host': '', // api host url
+    'client_id': '',
+    'client_secret': '',
+    'scopes': [],
+  },
 };
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
-var { createCart, createOrderFromCart,getCartDetails } = require('../lib/commercetool');
+const {createCart, createOrderFromCart, getCartDetails} = require('../lib/commercetool');
 
 module.exports = (app) => {
-	app.use(bodyParser.urlencoded({ extended: false }))
-	app.use(bodyParser.json());
-	app.use('/',require('../middlewares'));
-	app.use('/createOrder',createOrderFromCart);
-	app.use('/createCart',createCart);
-	app.use('/cart/:cartId/pay',getCartDetails);
-	app.use('/', require('./home'));
-	app.use('/category', require('./category'));
-	app.use('/product', require('./product'));
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+  app.use('/', require('../middlewares'));
+  app.use('/createOrder', createOrderFromCart);
+  app.use('/createCart', createCart);
+  app.use('/cart/:cartId/pay', getCartDetails);
+  app.use('/', require('./home'));
+  app.use('/category', require('./category'));
+  app.use('/product', require('./product'));
 };
