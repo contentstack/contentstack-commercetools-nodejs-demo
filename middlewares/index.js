15f20286bf6a99075733f7bd020e1903c19b6982
dinesh1234gowda <42273641+dinesh1234gowda@users.noreply.github.com>
2020-02-01 01:24:16 +0530

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,13 +2,6 @@ const express = require('express');
 const router = express.Router();
 const basicAuth = require('express-basic-auth');
 
-function a(req, res, next) {
-  basicAuth({
-    	users: {'admin': 'supersecret'},
-  });
-  next();
-}
-
-router.get('*', a, require('./load-partials'));
+router.get('*',require('./load-partials'));
 
 module.exports = router;
