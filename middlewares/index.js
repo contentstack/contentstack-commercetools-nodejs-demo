934443d6bc8db2a1c5c29293519da9aa1be44567
dinesh1234gowda <42273641+dinesh1234gowda@users.noreply.github.com>
2020-02-01 01:26:23 +0530

    changes

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,4 @@
 const express = require('express');
 const router = express.Router();
-const basicAuth = require('express-basic-auth');
-
 router.get('*',require('./load-partials'));
-
 module.exports = router;
