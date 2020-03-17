ec221258e356b265d9a554d7ed5a8f51888d2005
Dinesh Gowda <dinesh.gowda@contentstack.com>
2020-03-17 20:49:17 +0530

    changes

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,14 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const basicAuth = require('express-basic-auth')
-
-function a(req,res,next){
-	basicAuth({
-    	users: { 'admin': 'supersecret' }
-	})
-	next()
-}
-
-router.get('*', a,require('./load-partials'));
+router.get('*',require('./load-partials'));
 
 module.exports = router;
